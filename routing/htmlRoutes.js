ceb4a202ad7631c304efc31407cb063d103e9fa7
Carmen Owen <doctorsout@yahoo.com>
2018-08-15 16:00:13 -0400

    added html folder

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -1,10 +1,22 @@
 
 module.exports = function(app){
+    
     var path = require("path");
 
     app.get("/home", function(req, res){
-        res.sendFile(path.join(__dirname, "../public/home.html"));
+        res.sendFile(path.join(__dirname, "../public/html/home.html"));
     });
 
-    
+    app.get("/library", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/html/library.html"));
+    });
+
+    app.get("/wishlist", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/html/wishlist.html"));
+    });
+
+    app.get("/for-sale", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/html/for-sale.html"));
+    });
+
 };
