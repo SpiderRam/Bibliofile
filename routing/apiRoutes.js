ac6e389cd0cd9ec388ed8499041ea0a4658fac3e
Reeshida <reeshida@hotmail.com>
2018-08-14 20:20:40 -0400

    added node isbn api package

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,6 +1,7 @@
 module.exports = function(app){
     var db = require("../models");
     var request= require("request");
+    var isbn = require('node-isbn');
     app.get("/save", function(req, res){
         db.Book.create({
             title:"my first book"
@@ -10,23 +11,19 @@ module.exports = function(app){
         });
     })
     app.get("/getbooks", function(req, res){
-        request('http://www.omdbapi.com/?t=' + "Cars" + '&y=&plot=short&r=json', function (error, response, body) {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body); // Print the HTML for the Google homepage.
-    
-        var jsonData = JSON.parse(body);
-        console.log('Title:' + jsonData.Title);
-        console.log('Year:' + jsonData.Year);
-        console.log('Rated:' + jsonData.Rated);
-        console.log('IMDB Rating:' + jsonData.imdbRating);
-        console.log('Country:' + + jsonData.Country);
-        console.log('Language:' + jsonData.Language);
-        console.log('Plot:' + jsonData.Plot);
-        console.log('Actors:' + jsonData.Actors);
-        console.log('Rotten tomatoes rating:' + jsonData.tomatoRating);
-        
-    });
+        isbn.resolve('1400201659', function (err, book) {
+            if (err) {
+                console.log('Book not found', err);
+            } else {
+                console.log('Book found %j', book);
+                res.json(book);
+            }
+        });
+       
         })
+
+
 }
+
+
    
