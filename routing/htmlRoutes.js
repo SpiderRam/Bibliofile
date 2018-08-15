37906f122b67c1fc7d23750e86066e9a30cb829e
Carmen Owen <doctorsout@yahoo.com>
2018-08-15 15:54:46 -0400

    reworked file structure, added image folder, css folder and file, model file for each endpoint

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -1,7 +1,10 @@
 
 module.exports = function(app){
     var path = require("path");
+
     app.get("/home", function(req, res){
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
+
+    
 };
