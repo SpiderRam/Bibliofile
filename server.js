7995f444ed3c7ba8bb5b278f92a93c274f7a3a19
Carmen Owen <doctorsout@yahoo.com>
2018-08-21 20:25:03 -0400

    isbn npm working

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ var app = express();
 var bodyParser = require("body-parser");
 var db = require("./models");
 var isbn = require('node-isbn');
-var passport = require('passport');
-var session = require('express-session');
-var env = require('dotenv').load();
-var exphbs = require('express-handlebars');
+var cors = require('cors');
+
 
 //Models
 var models = require("./models");
@@ -19,57 +17,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ type: 'application/*+json' }));
 app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
 app.use(bodyParser.text({ type: 'text/html' }));
+app.use(cors());
 
 app.use(express.static("public"));
-
-// For Passport
- 
-// app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
- 
-// app.use(passport.initialize());
- 
-// app.use(passport.session()); // persistent login sessions
-
-
-// //For Handlebars
-// app.set('views', './app/views')
-// app.engine('hbs', exphbs({
-//   extname: '.hbs'
-// }));
-// app.set('view engine', '.hbs');
-
-// app.get('/', function(req, res) {
- 
-//   res.send('Welcome to Passport with Sequelize');
-
-// });
-isbn.resolve('', function (err, book) {
-    if (err) {
-        console.log('Book not found', err);
-    } else {
-        console.log('Book found %j', book);
-    }
-});
-// var express = require('express'),
-//     app = express(),
-//     http = require('http'),
-//     server = http.createServer(app),
-//     xmlparser = require('express-xml-bodyparser');
- 
-// // .. other middleware ... 
-// app.use(express.json());
-// app.use(express.urlencoded());
-// app.use(xmlparser());
-// // ... other middleware ... 
- 
-// app.post('/receive-xml', function(req, res, next) {
- 
-//   // req.body contains the parsed xml
- 
-// });
- 
-// server.listen(1337);
- 
 
 //Sync Database
 models.sequelize.sync().then(function() {
@@ -105,3 +55,52 @@ db.sequelize.sync().then(function() {
   
   
 
+
+
+
+
+
+
+
+  // For Passport
+ 
+// app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+ 
+// app.use(passport.initialize());
+ 
+// app.use(passport.session()); // persistent login sessions
+
+
+// //For Handlebars
+// app.set('views', './app/views')
+// app.engine('hbs', exphbs({
+//   extname: '.hbs'
+// }));
+// app.set('view engine', '.hbs');
+
+// app.get('/', function(req, res) {
+ 
+//   res.send('Welcome to Passport with Sequelize');
+
+// });
+
+// var express = require('express'),
+//     app = express(),
+//     http = require('http'),
+//     server = http.createServer(app),
+//     xmlparser = require('express-xml-bodyparser');
+ 
+// // .. other middleware ... 
+// app.use(express.json());
+// app.use(express.urlencoded());
+// app.use(xmlparser());
+// // ... other middleware ... 
+ 
+// app.post('/receive-xml', function(req, res, next) {
+ 
+//   // req.body contains the parsed xml
+ 
+// });
+ 
+// server.listen(1337);
+ 
