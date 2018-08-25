c6597585037409af24899c5efe7e4bbd10fe9ae1
Carmen Owen <doctorsout@yahoo.com>
2018-08-25 11:42:57 -0400

    delete working on for sale

--- public/js/for-sale-logic.js
+++ public/js/for-sale-logic.js
@@ -1,15 +1,90 @@
-$(document).ready(function(){      
+const handleDeleteClick = function (book) {
+    event.preventDefault();
+    console.log("Trying to delete " + book.id);
+
+    $.ajax({
+        type: "DELETE",
+        url: "/forSale-delete/" + book.id
+    }).then(function(response) {
+        generateForSaleContent();
+        console.log(JSON.stringify(response));
+        console.log("Deleted " + book.id + " from forsale");
+    });
+};
+
+const generateForSaleContent = function() {
     var userID = sessionStorage.userID;
     $.ajax({
         type: "GET",
         url: "/for-sale/" + userID
     }).then(function(response){
-        
+        const forSaleContents = $("#forSaleContents");
+        forSaleContents.empty();
+    
         for (i = 0; i < response.length; i++) {
-            var book = response[i];
-        
-        $("#forSaleContents").append("<li class='forSaleEntry' id='forSaleId" + book.id +  "'><img src='../images/icon-book-delete.png' class='forSaleDelete' title='Delete' id='deleteBook" + book.id + "'><img src='../images/icon-book-edit.png' class='forSaleEdit' title='Change Price' id='editBook" + book.id + "'><img src='../images/icon-request-info.png' class='minPriceUpdate' title='Find Buyers' id='updatePriceOf" + book.Min_Price + "'><span class='underline'>" + book.Title + "</span> <span class='bold'> ✒︎ </span> " + book.Author + "<span class='bold'> ✒︎ </span>Min Price: " + book.Min_Price + "</li>");
+            const book = response[i];
+    
+            const listItem = $("<li>")
+                .addClass("forSaleEntry")
+                .attr("id", "forSaleId" + book.id)
+                .text(book.Author);
+    
+            const deleteIcon = $("<img>")
+                .addClass("forSaleDelete")
+                .attr("src", "../images/icon-book-delete.png")
+                .attr("title", "Delete")
+                .attr("id", "deleteForSaleBook" + book.id);
+
+            const editIcon = $("<img>")
+                .addClass("forSaleEdit")
+                .attr("src", "../images/icon-book-edit.png")
+                .attr("title", "Change Price")
+                .attr("id", "editForSaleBook" + book.id);
+                
+            const searchIcon = $("<img>")
+                .addClass("minPriceUpdate")
+                .attr("src", "../images/icon-request-info.png")
+                .attr("title", "Find Buyers")
+                .attr("id", "updatePriceOf" + book.id);  
+    
+            deleteIcon.on("click", function() {
+                handleDeleteClick(book);
+            });
+    
+            const titleSpan = $("<span>")
+                .addClass("underline")
+                .text(book.Title);
+    
+            const symbolSpan = $("<span>")
+                .addClass("bold")
+                .text(" ✒︎ ");
+    
+            listItem.prepend([deleteIcon, editIcon, searchIcon, titleSpan, symbolSpan]);
+            forSaleContents.append(listItem);
         }
     });
+};
 
+
+$(document).ready(function(){      
+    generateForSaleContent();
 });
+
+
+
+
+// $(document).ready(function(){      
+//     var userID = sessionStorage.userID;
+//     $.ajax({
+//         type: "GET",
+//         url: "/for-sale/" + userID
+//     }).then(function(response){
+        
+//         for (i = 0; i < response.length; i++) {
+//             var book = response[i];
+        
+//         $("#forSaleContents").append("<li class='forSaleEntry' id='forSaleId" + book.id +  "'><img src='../images/icon-book-delete.png' class='forSaleDelete' title='Delete' id='deleteForSaleBook" + book.id + "'><img src='../images/icon-book-edit.png' class='forSaleEdit' title='Change Price' id='editForSaleBook" + book.id + "'><img src='../images/icon-request-info.png' class='minPriceUpdate' title='Find Buyers' id='updatePriceOf" + book.Min_Price + "'><span class='underline'>" + book.Title + "</span> <span class='bold'> ✒︎ </span> " + book.Author + "<span class='bold'> ✒︎ </span>Min Price: " + book.Min_Price + "</li>");
+//         }
+//     });
+
+// });
